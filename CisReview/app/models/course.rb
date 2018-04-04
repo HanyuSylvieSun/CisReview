f56589789fdb292d7327012836c3a89a924895df
Hanyu Sun <sunhanyu@seas.upenn.edu>
2018-04-04 01:01:15 -0400

    instructor&courses everything work

--- CisReview/app/models/course.rb
+++ CisReview/app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
   belongs_to :instructor, optional: :true
+  validates :name, presence: :true
+  validates :number, presence: :true
 end
--- CisReview/app/models/instructor.rb
+++ CisReview/app/models/instructor.rb
@@ -1,3 +1,16 @@
 class Instructor < ApplicationRecord
 	has_many :courses
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+
+	validate :f_capitalize
+    validate :l_capitalize
+
+    def f_capitalize
+      errors.add(:first_name, 'must be capitalized') if first_name && first_name[0] != first_name[0].upcase
+    end
+
+    def l_capitalize
+      errors.add(:last_name, 'must be capitalized') if last_name && last_name[0] != last_name[0].upcase
+    end
 end
