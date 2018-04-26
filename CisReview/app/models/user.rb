2dfb368a8aefd5107a366471df124c45c3093e81
Hanyu Sun <sunhanyu@seas.upenn.edu>
2018-04-25 21:38:17 -0400

    image added to user and instructor

--- CisReview/app/models/user.rb
+++ CisReview/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :courses
+  has_attached_file :image, styles: { medium: "300x300#"}, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- CisReview/db/schema.rb
+++ CisReview/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426005029) do
+ActiveRecord::Schema.define(version: 20180426012732) do
 
   create_table "courses", force: :cascade do |t|
     t.string "number"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20180426005029) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,6 +47,10 @@ ActiveRecord::Schema.define(version: 20180426005029) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
