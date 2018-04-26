162797df2f9b5fb978cacf6aef995580138ffda5
Hanyu Sun <sunhanyu@seas.upenn.edu>
2018-04-25 22:13:12 -0400

    dependency added

--- CisReview/app/models/instructor.rb
+++ CisReview/app/models/instructor.rb
@@ -1,7 +1,7 @@
 class Instructor < ApplicationRecord
-   has_attached_file :image, styles: { medium: "300x300#"}, default_url: "/images/:style/missing.png"
+   has_attached_file :image, styles: { medium: "200x200#"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-	has_many :courses
+	has_many :courses, dependent: :delete_all
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 
--- CisReview/app/models/user.rb
+++ CisReview/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :courses
-  has_attached_file :image, styles: { medium: "300x300#"}, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "200x200#"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   
   # Include default devise modules. Others available are:
